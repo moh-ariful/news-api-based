710bef0eef7e2e44509296614586c93441b25e94
Moh Ariful <nextkoding@gmail.com>
2022-06-26 10:50:37 +0700

    Berhasil tayang dgn styling minimalis

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from newsapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,22 @@
+from turtle import title
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def index(request):
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=3dfd703d14a4b403901778bc5730d9db&languages=en')
+    res = r.json()
+    data = res['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i['image'])
+        url.append(i['url'])
+    news = zip(title, description, image, url)
+   
+    return render(request, 'newsapp/index.html', {'news':news})
